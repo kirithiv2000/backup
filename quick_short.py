e38ea7dc05295a12c3e8bc35627851a198200d6b
Kirithiv R <52394696+kirithiv2000@users.noreply.github.com>
2020-02-01 18:49:02 +0530

    Update quick_short.py

--- quick_short.py
+++ quick_short.py
@@ -1,29 +1,29 @@
-# def pivots_position(list1,first,last):
-# 	pivot=list1[first]
-# 	left=first+1
-# 	right=last
-# 	while 1:
-# 		while left<=right and list1[left]<=pivot:
-# 			left+=1
-# 		while right>=left and pivot<=list1[right]:
-# 			right-=1
-# 		if left>right:
-# 			list1[first],list1[right]=list1[right],list1[first]
-# 			break
-# 		else:	
-# 			list1[left],list1[right]=list1[right],list1[left]
-# 	return right
+ def pivots_position(list1,first,last):
+ 	pivot=list1[first]
+ 	left=first+1
+ 	right=last
+ 	while 1:
+ 		while left<=right and list1[left]<=pivot:
+ 			left+=1
+ 		while right>=left and pivot<=list1[right]:
+ 			right-=1
+ 		if left>right:
+ 			list1[first],list1[right]=list1[right],list1[first]
+ 			break
+ 		else:	
+ 			list1[left],list1[right]=list1[right],list1[left]
+ 	return right
 
-# def quick_sort(list1,first,last):
-# 	if first<last:
-# 		p=pivots_position(list1,first,last)
-# 		quick_sort(list1,p+1,last)
-# 		quick_sort(list1,first,p-1)
+ def quick_sort(list1,first,last):
+ 	if first<last:
+ 		p=pivots_position(list1,first,last)
+ 		quick_sort(list1,p+1,last)
+ 		quick_sort(list1,first,p-1)
 		
-# import random
-# a=[random.randint(1,100) for i in range(100)]
-# quick_sort(a,0,len(a)-1)
-# print(a)
+ import random
+ a=[random.randint(1,100) for i in range(100)]
+ quick_sort(a,0,len(a)-1)
+ print(a)
 
 
 # a='[({{{{}}}})][({}())}]'
@@ -151,15 +151,15 @@ print (result) '''
 
 
 
-def range(startingNumber,endingNumber=10,differanceNumber=1):
-	if startingNumber>endingNumber-1:
-		return []
-	value=range(startingNumber+differanceNumber,endingNumber,differanceNumber)
-	value.insert(0,startingNumber)
-	return value
-print(range(1,6))   #[1, 2, 3, 4, 5]
-print(range(0,20,))#[0, 2, 4, 6, 8]
-print(range(1,10,2))#[1, 3, 5, 7, 9]
+#def range(startingNumber,endingNumber=10,differanceNumber=1):
+#	if startingNumber>endingNumber-1:
+#		return []
+#	value=range(startingNumber+differanceNumber,endingNumber,differanceNumber)
+#	value.insert(0,startingNumber)
+#	return value
+#print(range(1,6))   #[1, 2, 3, 4, 5]
+#print(range(0,20,))#[0, 2, 4, 6, 8]
+#print(range(1,10,2))#[1, 3, 5, 7, 9]
 
 # if i in range(10)
 
